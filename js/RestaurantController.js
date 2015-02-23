c3a575eb75485360935f8fed755ece72a0045e98
Joey Sandine and Sean Holstrom <jasandine@gmail.com>
2015-02-23 12:17:46 -0800

    Add restaurant type and location functionality

--- js/RestaurantController.js
+++ js/RestaurantController.js
@@ -1,9 +1,16 @@
 restaurantList.controller('RestaurantCtrl', function RestaurantCtrl($scope) {
   $scope.restaurants = [];
   $scope.addRestaurant = function() {
-    $scope.restaurants.push({ name: $scope.restaurantName, price: $scope.restaurantPrice });
+    $scope.restaurants.push({ name: $scope.restaurantName,
+                              price: $scope.restaurantPrice,
+                              type: $scope.restaurantType,
+                              location: $scope.restaurantLocation
+                              });
     $scope.restaurantName = null;
     $scope.restaurantPrice = null;
+    $scope.restaurantType = null;
+    $scope.restaurantLocation = null;
+
   };
   $scope.deleteRestaurant = function(restaurant) {
     var index = $scope.restaurants.indexOf(restaurant)
