6d89632d94124f8f3ebe0c131d9571b8c26280bc
Joey Sandine and Sean Holstrom <basementbebop@gmail.com>
2015-02-23 11:09:44 -0800

    Add restaurant edit functionality

--- js/RestaurantController.js
+++ js/RestaurantController.js
@@ -4,5 +4,8 @@ restaurantList.controller('RestaurantCtrl', function RestaurantCtrl($scope) {
    $scope.restaurants.push({ name: $scope.restaurantName });
    $scope.restaurantName = null;
  };
-  $scope.deleteRestaurant = function(restaurant)
+  $scope.deleteRestaurant = function(restaurant) {
+    var index = $scope.restaurants.indexOf(restaurant)
+    $scope.restaurants.splice(index, 1);
+  };
 });
