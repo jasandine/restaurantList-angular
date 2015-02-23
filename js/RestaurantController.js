5874ab8549008be84875e20130e3066a7ff9fb34
Joey Sandine and Sean Holstrom <jasandine@gmail.com>
2015-02-23 11:22:38 -0800

    Add restaurant price functionality

--- js/RestaurantController.js
+++ js/RestaurantController.js
@@ -1,9 +1,10 @@
 restaurantList.controller('RestaurantCtrl', function RestaurantCtrl($scope) {
- $scope.restaurants = [];
- $scope.addRestaurant = function() {
-   $scope.restaurants.push({ name: $scope.restaurantName });
-   $scope.restaurantName = null;
- };
+  $scope.restaurants = [];
+  $scope.addRestaurant = function() {
+    $scope.restaurants.push({ name: $scope.restaurantName, price: $scope.restaurantPrice });
+    $scope.restaurantName = null;
+    $scope.restaurantPrice = null;
+  };
   $scope.deleteRestaurant = function(restaurant) {
     var index = $scope.restaurants.indexOf(restaurant)
     $scope.restaurants.splice(index, 1);
